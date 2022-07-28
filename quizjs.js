d674db18391c2c460628729f1988cc70457889d5
ronbrixD2001 <110148805+ronbrixD2001@users.noreply.github.com>
2022-07-28 13:50:47 +0800

    Add files via upload

---
+++ quizjs.js
@@ -0,0 +1,24 @@
+function check() {
+    var c=0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    var q6=document.quiz.question6.value;
+    var q7=document.quiz.question7.value;
+    var q8=document.quiz.question8.value;
+    var q9=document.quiz.question9.value;
+    var q10=document.quiz.question10.value;
+    if (q1=="Discourse Competence") {c++}
+    if (q2=="Interaction") {c++}
+    if (q3=="Contextualizatio") {c++}
+    if (q4=="LSpiral Progression") {c++}
+    if (q5=="develops thinking and language through interactive learning") {c++}
+    if (q6==" A holistic view of language") {c++}
+    if (q7=="Developmental appropriateness") {c++}
+    if (q8=="Holistic Assessment") {c++}
+    if (q9=="UNDERSTANDING LANGUAGE") {c++}
+    if (q10=="A holistic view of language") {c++}    
+        document.write (c);
+}
